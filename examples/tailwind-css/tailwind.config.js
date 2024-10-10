6b87b16d03812274b10bfeb0eabbb39ba11e698e
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2024-10-10 17:14:26 +0700

    Create example for tailwind-css

---
+++ examples/tailwind-css/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {
+  extend: {
+    colors: {
+      primary: '#1DA1F2', // Custom primary color
+    },
+  },
+};
+export const plugins = [];

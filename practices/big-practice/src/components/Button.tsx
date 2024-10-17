c7a8e845c958ebcf8fa7245fb2c1f7758198b967
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2024-10-17 09:43:02 +0700

    Create header page #2

---
+++ practices/big-practice/src/components/Button.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface ButtonProps {
+  label: string;
+  onClick: () => void;
+  disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  label,
+  onClick,
+  disabled = false
+}) => {
+  const buttonClasses = disabled
+    ? 'bg-gray-300 text-gray-500 cursor-not-allowed'
+    : 'bg-[#F4F5F9] text-[#1CA1C1] hover:bg-slate-300';
+
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`px-4 py-2 border-none mr-7 ${buttonClasses}`}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;

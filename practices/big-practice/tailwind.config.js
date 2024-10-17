161477622c3ec72077cbbd16977141835983212e
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2024-10-17 09:13:06 +0700

    Init project #1

--- practices/big-practice/tailwind.config.js
+++ practices/big-practice/tailwind.config.js
@@ -1,9 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];

d34d353c23332d4446f79ad693e6401b348b23a1
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2024-10-23 17:29:40 +0700

    Update db and config

--- practices/big-practice/tailwind.config.js
+++ practices/big-practice/tailwind.config.js
@@ -3,6 +3,10 @@ export const content = [
   "./src/**/*.{js,ts,jsx,tsx}",
 ];
 export const theme = {
-  extend: {},
+  extend: {
+    colors: {
+      customBorder: '#DADEE0',
+    },
+  },
 };
 export const plugins = [];

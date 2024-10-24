4567e7e605c5196d2e9a08274e13d1758e4709c4
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2024-10-24 16:06:55 +0700

    Fix code style and update db

--- practices/big-practice/src/types/aggrid.tsx
+++
@@ -1,9 +0,0 @@
-import { ColDef, RowClassParams, RowClickedEvent } from "ag-grid-community";
-
-export interface DataGridProps<T> {
-  rowData: T[];
-  columnDefs: ColDef<T>[];
-  onRowClicked?: (event: RowClickedEvent) => void;
-  getRowClass?: (params: RowClassParams) => string;
-  rowHeight?: number;
-};

bdafebc1b4df67938d1027829b9bc7d1b1b78d6d
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2024-10-29 09:16:12 +0700

    Update config

--- practices/big-practice/vite.config.ts
+++ practices/big-practice/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Base alias for `src`
+      '@types': path.resolve(__dirname, 'src/types') // Alias for `src/types`
+    },
+  },
+});

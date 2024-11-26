b3cdbcf229396ab77ddf5f915cb316667e6ce8b3
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2024-11-26 17:56:07 +0700

    Fix code style

--- practices/big-practice/src/App.test.tsx
+++ practices/big-practice/src/App.test.tsx
@@ -1,19 +1,19 @@
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import App from "./App";
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from './App';
 
 // Mock Dashboard component
-jest.mock("./pages/Dashboard/Dashboard", () => ({
-  ...jest.requireActual("./pages/Dashboard/Dashboard"),
-  default: () => <div data-testid="dashboard-component">Mocked Dashboard</div>,
+jest.mock('./pages/Dashboard/Dashboard', () => ({
+  ...jest.requireActual('./pages/Dashboard/Dashboard'),
+  default: () => <div data-testid='dashboard-component'>Mocked Dashboard</div>,
 }));
 
 jest.mock('@/config', () => ({
   API_BASE_URL: 'http://localhost:3001',
 }));
 
-describe("App Component", () => {
-  it("matches snapshot for App", () => {
+describe('App Component', () => {
+  it('matches snapshot for App', () => {
     const { container } = render(<App />);
 
     expect(container).toMatchSnapshot();

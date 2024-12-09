a1ea14af42b73d38ea507c2171154244ddde6e6d
Vinh Huynh <vinh.huynhduc@asnet.com.vn>
2024-12-09 10:49:12 +0700

    Update config storybook

--- practices/big-practice/.storybook/main.ts
+++ practices/big-practice/.storybook/main.ts
@@ -4,17 +4,14 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-onboarding",
+    "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
-    "@storybook/addon-docs",
-    "@storybook/addon-controls",
-    "@storybook/addon-actions",
   ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
-  }
+  },
 };
-
 export default config;
